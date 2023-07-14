a8475f2fa8b41aafbb2cd0965bf9563b24941b21
Jon Watkins <jonwatkins86@gmail.com>
2023-07-14 23:22:29 +0100

    updated lib-react-ui version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import 'lib-react-ui/dist/styles.css'
+import 'lib-react-ui/dist/index.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
